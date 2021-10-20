31314433428d8330d477ac85497b91c3c5e42c7f
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2021-10-20 12:29:07 +0200

    Add files via upload

---
+++ Boletin2-1.java
@@ -0,0 +1,19 @@
+package boletin2.pkg1;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin21 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float base= 4.0f, altura= 3;
+        
+        float área= base*altura/2;
+        
+        
+        System.out.println("O valor da área é "+ área+ ".");
+    }
+    
+}
